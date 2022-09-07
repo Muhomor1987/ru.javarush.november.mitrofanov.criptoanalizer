ec3408ed3cdd720521eee18c3e0db80531a0f837
Mitrofanov Mikhail <z.pack@mail.ru>
2022-09-07 15:34:58 +0300

    The class "Alphabet" is written

---
+++ src/main/java/javarush/criptoanalizer/IO/StreamAlphabet.java
@@ -0,0 +1,9 @@
+package javarush.criptoanalizer.IO;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class StreamAlphabet {
+    BufferedReader readerAlphabet = new BufferedReader(new InputStreamReader(new FileInputStream(javarush.criptoanalizer.files.), "UTF-8"))
+}
